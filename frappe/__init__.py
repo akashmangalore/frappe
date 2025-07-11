6e02fc7c939b2b244b9a0336b9b7e9815726f9b7
Akash M <122703615+akashmangalore@users.noreply.github.com>
2025-07-11 14:32:05 +0530

    Update __init__.py

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -54,7 +54,7 @@ from .utils.jinja import (
 	render_template,
 )
 
-__version__ = "v16.0.0-dev.20250711"
+__version__ = "16.0.0-dev.20250711"
 __title__ = "Frappe Framework"
 
 if TYPE_CHECKING:  # pragma: no cover
